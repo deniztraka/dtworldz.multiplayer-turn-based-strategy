dc2402905d6f7e93abb79a0f6beacd28813c48b5
Deniz Traka <deniz.traka@deptagency.com>
2023-11-27 12:52:33 +0100

    test

--- dtworldz.client/src/backend.ts
+++ dtworldz.client/src/backend.ts
@@ -2,7 +2,7 @@
 // export const BACKEND_HTTP_URL = 'ws://dtworldz-server.onrender.com:10000'
 
 export const BACKEND_URL = (window.location.href.indexOf("localhost") === -1)
-    ? `ws://dtworldz-server.onrender.com:10000`
+    ? `wss://dtworldz-server.onrender.com:10000`
     : "ws://localhost:2594"
 
 export const BACKEND_HTTP_URL = BACKEND_URL.replace("ws", "http");
