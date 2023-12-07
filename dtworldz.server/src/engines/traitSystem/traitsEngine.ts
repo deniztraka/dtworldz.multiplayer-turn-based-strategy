61fc4db8254ad4e97bce182d318c7ad04ec01530
Deniz Traka <deniz.traka@deptagency.com>
2023-12-07 01:45:30 +0100

    Minor updates to allow some characters move on mountain and forest tiles

--- dtworldz.server/src/engines/traitSystem/traitsEngine.ts
+++ dtworldz.server/src/engines/traitSystem/traitsEngine.ts
@@ -5,8 +5,9 @@ import { Trait } from "./traits";
 export class TraitsEngine {
     static requirements: Map<Trait, { attribute: Attributes, minimumVal: number }[]> = new Map([
         [Trait.Swimming, [{ attribute: Attributes.Dexterity, minimumVal: 20 }, { attribute: Attributes.Strength, minimumVal: 5 }]],
-        [Trait.Climbing, [{ attribute: Attributes.Strength, minimumVal: 30 }, { attribute: Attributes.Dexterity, minimumVal: 30 }]],
-        [Trait.Pathfinding, [{ attribute: Attributes.Intelligence, minimumVal: 15 }, { attribute: Attributes.Dexterity, minimumVal: 20 }]],
+        [Trait.Climbing, [{ attribute: Attributes.Strength, minimumVal: 8 }, { attribute: Attributes.Intelligence, minimumVal: 5 }]],
+        [Trait.Pathfinding, [{ attribute: Attributes.Intelligence, minimumVal: 5 }, { attribute: Attributes.Dexterity, minimumVal: 8 }]],
+        [Trait.Lumberjack, [{ attribute: Attributes.Strength, minimumVal: 4 }, { attribute: Attributes.Dexterity, minimumVal: 4 }]],
         // ... other requirements
     ]);
 
